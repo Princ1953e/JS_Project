91ca5d8e5284486336c3b7e37d65b899b805f230
Princ1953e <princ1953e@gmail.com>
2024-06-13 18:15:05 +0530

    JS_PR

--- PR_1/script.js
+++ PR_1/script.js
@@ -1,25 +1,29 @@
 class school {
   student(name, contact, email) {
-    document.getElementById("mn1").innerHTML = `My Name Is ${name} `;
+    document.getElementById(
+      "mn1"
+    ).innerHTML = `My Name Is ${name}<br/> <br/>  `;
     document.getElementById(
       "mc1"
-    ).innerHTML = `My Contact Number Is ${contact}`;
-    document.getElementById("me1").innerHTML = `My Email Address Is ${email}`;
+    ).innerHTML = `My Contact Number <br/> ${contact}<br/> <br/> `;
+    document.getElementById(
+      "me1"
+    ).innerHTML = `My Email Address <br/> ${email}`;
   }
   faculty(name, contact, email) {
-    document.getElementById("fn1").innerHTML = `My Faculty Name Is ${name} `;
+    document.getElementById("fn1").innerHTML = `My Faculty Name <br/> ${name} `;
     document.getElementById(
       "fc1"
-    ).innerHTML = `My Faculty Contact Number Is ${contact}`;
+    ).innerHTML = `My Faculty Contact Number ${contact}<br/><br/>`;
     document.getElementById(
       "fe1"
-    ).innerHTML = `My Faculty Email Address Is ${email}`;
+    ).innerHTML = `My Faculty Email Address ${email}`;
   }
   peon(name, contact, email) {
-    document.getElementById("pn1").innerHTML = `Peon Name Is ${name} `;
+    document.getElementById("pn1").innerHTML = `Peon Name Is <br/> ${name} `;
     document.getElementById(
       "pc1"
-    ).innerHTML = `Peon Contact Number Is ${contact}`;
+    ).innerHTML = `Peon Contact Number Is <br/> ${contact} <br/> <br/>`;
     document.getElementById("pe1").innerHTML = `Peon Email Address Is ${email}`;
   }
 }
@@ -27,6 +31,6 @@ class school {
 let s1 = new school();
 s1.student("Prince", 9978617374, "princ1953e@gmail.com");
 let f1 = new school();
-f1.faculty("K_G Sir", 8802377949, "k_g01@gmail.com");
+f1.faculty("K_G SIR", 8802377949, "k_g01@gmail.com");
 let p1 = new school();
 p1.peon("Manish_Bhai", 9937849272, "manu_33@gmail.com");
